cb63849b4887ee4334b722f5ae0c566660cd1b55
Arnav Dixit <arnavdixit022611@gmail.com>
2026-06-03 18:06:58 +0530

    Added a timer and am working on smoother animation.

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
-import { useEffect, useState } from "react";
+import { use, useEffect, useState } from "react";
 import Field from "./Field";
 import Robot from "./Robot";
 
 type Point = {
   x: number;
   y: number;
+  t: number;
 };
-
+  
 function App() {
   const [path, setPath] = useState<Point[]>([]);
   const [currentPoint, setCurrentPoint] = useState(0);
   const [team, setTeam] = useState("254");
   const [auto, setAuto] = useState("left");
+  const [playing, setPlaying] = useState(true);
+  const [time, setTime] = useState(0);
+  const [robotX, setRobotX] = useState(0);
+  const [robotY, setRobotY] = useState(0);
   // Load path from JSON
 useEffect(() => {
   console.log(
@@ -42,18 +47,31 @@ useEffect(() => {
   // Animate robot
   useEffect(() => {
     if (path.length === 0) return;
-
+    if (!playing) return;
     const interval = setInterval(() => {
       setCurrentPoint((prev) => {
         if (prev >= path.length - 1) {
-          return 0;
+        setPlaying(false);
+        return prev;
         }
         return prev + 1;
       });
+      setCurrentPoint((prev) => {
+  if (prev >= path.length - 1) {
+    setPlaying(false);
+    return prev;
+  }
+
+  const next = prev + 1;
+
+  setTime(path[next].t);
+
+  return next;
+});
     }, 300);
 
     return () => clearInterval(interval);
-  }, [path.length]);
+  }, [path.length, playing]);
 
   // Wait until JSON is loaded
   if (path.length === 0) {
@@ -67,7 +85,9 @@ useEffect(() => {
   return (
     <div>
       <h1>FRC Auton Viewer</h1>
-
+        <p>
+  Time: {time.toFixed(1)}s / {path[path.length - 1].t.toFixed(1)}s
+</p>
       <p>Path Length: {path.length}</p>
       <p>Current Point: {currentPoint}</p>
       <select
@@ -85,6 +105,23 @@ useEffect(() => {
   <option value="center">Center Auto</option>
   <option value="right">Right Auto</option>
 </select>
+      <button onClick={() => setPlaying(true)}>
+  Play
+</button>
+
+<button onClick={() => setPlaying(false)}>
+  Pause
+</button>
+
+<button
+  onClick={() => {
+    setPlaying(false);
+    setCurrentPoint(0);
+    setTime(0);
+  }}
+>
+  Reset
+</button>
       <Field>
         <polyline
           points={points}
